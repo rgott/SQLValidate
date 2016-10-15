9445ea9cf0317f932301a5c89e7098c41aee089c
Rob <Rob@ROBBIE>
2016-10-15 09:45:14 -0400

    cleaned up code and functions

--- Sql_Injection/Sql_Injection/Controllers/HomeController.cs
+++ Sql_Injection/Sql_Injection/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Sql_Injection.Models;
+﻿using System;
+using Sql_Injection.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -9,7 +10,6 @@ namespace Sql_Injection.Controllers
         /// <summary>
         /// Function gets id to check with database
         /// </summary>
-        /// <param name="id"></param>
         /// <returns></returns>
         public ActionResult Index()
         {
@@ -22,21 +22,21 @@ namespace Sql_Injection.Controllers
             if (model?.sqlQuery != null)
             {
                 // TODO: change method naming convention
-                model.isValidQuery = validateSqlQuery(model.sqlQuery);
+                model.isValidQuery = ValidateSqlQuery(model.sqlQuery);
             }
             return View(model);
         }
 
-        private bool? validateSqlQuery(string sqlQuery)
+        private bool? ValidateSqlQuery(string sqlQuery)
         {
             // equalize string for parsing
             sqlQuery = sqlQuery.ToLower();
 
-            int indexOfWhereClause = sqlQuery.IndexOf("where"); // reduce the amount tested
+            var indexOfWhereClause = sqlQuery.IndexOf("where", StringComparison.Ordinal); // reduce the amount tested
             if (indexOfWhereClause == -1) // if no where clause the query must be logically secure
                 return true;
-            else
-                indexOfWhereClause += "where".Length;
+
+            indexOfWhereClause += "where".Length;
 
             // get everything after "where" clause
             sqlQuery = sqlQuery.Substring(indexOfWhereClause, sqlQuery.Length - indexOfWhereClause);
@@ -45,96 +45,29 @@ namespace Sql_Injection.Controllers
             sqlQuery = sqlQuery
                 .Replace(";", "")
                 .Replace("\"", "'");
-            return isValidExpression(getExpressionListFromQuery(sqlQuery));
-            //return validateSqlQueryFull(sqlQuery);
+            return IsValidExpression(sqlQuery);
         }
 
-        private List<string> getExpressionListFromQuery(string sqlQuery)
+        private bool? IsValidExpression(string sqlQuery)
         {
-            //TODO: remove once tested multiSplit
-            
-            //SortedSet<string> list = new SortedSet<string>();
-            // get all possible security flaws
-            // find or statements
-            // find operators between statements
-            // "operator and operator or operator"
+            List<string> expressionList = sqlQuery.MultiSplit(new string[] { "and", "or" });
 
-            //int prevIndex = 0;
-            //int index;
-
-
-            //while ((index = sqlQuery.IndexOf("or", prevIndex + 1)) != -1)
-            //{
-            //    // get left side
-            //    int tempV = prevIndex + "or".Length;
-            //    string left = sqlQuery.Substring(tempV, index - tempV);
-            //    int indexAND = left.LastIndexOf("and");
-            //    if (indexAND != -1)
-            //    {
-            //        int tmpLen = indexAND + "and".Length;
-            //        left = left.Substring(tmpLen, left.Length - tmpLen);
-            //    }
-
-            //    // get right side
-            //    int tmpV = index + "or ".Length;
-            //    string nextToken = sqlQuery.Substring(tmpV, sqlQuery.Length - tmpV);
-            //    string right = nextToken;
-            //    int RndexAND = right.IndexOf("and");
-            //    int RndexOR = right.IndexOf("or", 1);
-
-            //    RndexAND = (RndexAND == -1) ? 0 : RndexAND;
-            //    RndexOR = (RndexOR == -1) ? 0 : RndexOR;
-            //    int cndex = (RndexAND < RndexOR) ? RndexAND : RndexOR;
-            //    cndex = (cndex == 0) ? right.Length :cndex;
-            //    right = right.Substring(0, cndex);
-
-            //    list.Add(left.Trim());
-            //    list.Add(right.Trim());
-            //    prevIndex = index;
-            //}
-            List<string> list = sqlQuery.MultiSplit(new string[] { "and", "or" });
-            return list;
-        }
-        
-        private bool? isValidExpression(List<string> expressionList)
-        {
-            bool test = false;
             string[] stdOperaterToken = { "=", "like","not like", "<>", "!=", ">", "<", ">=", "<=" };
             foreach (var item in expressionList)
             {
-                test = false;
+                var expressionHasOnlyConst = false;
                 foreach (var operand in item.MultiSplit(stdOperaterToken))
                 {
-                    //
-                    if(isValidSqlExpression(operand))
-                    {
-                        test = true;
-                    }
+                    if(!HasSqlConstants(operand))
+                        expressionHasOnlyConst = true;
                 }
-                if (!test)
-                {
+                if (!expressionHasOnlyConst)
                     return false;
-                }
             }
-            // obsolete
-            //foreach (var item in expressionList)
-            //{
-            //    string stdOperator = stdOperaterToken.Find(m => item.Contains(m));
-            //    int index = item.IndexOf(stdOperator);
-            //    //get left of index
-            //    string left = item.Substring(0, index);
-
-            //    // get right of index without operator
-            //    int val = index + stdOperator.Length;
-            //    string right = item.Substring(val, item.Length - val);
-
-            //    if(isValidSqlExpression(left) && isValidSqlExpression(right))
-            //        return false;
-            //}
             return true;
         }
 
-        private bool isValidSqlExpression(string value)
+        private bool HasSqlConstants(string value)
         {
             const string quoteMark = "'";
 
@@ -152,19 +85,15 @@ namespace Sql_Injection.Controllers
             }
             else if (value.Contains(quoteMark))
             { // then is string => constant
-                return false;
+                return true;
             }
             else
             {
                 double placeHold; // only used to satisfy function parameter
                 if (double.TryParse(value, out placeHold))
-                    return false; // if integer => constant
+                    return true; // if integer => constant
             }
-            return true;
-        }
-        private bool isValidRange(string[] array, int minTest, int maxText)
-        {
-            return minTest >= 0 && maxText < array.Length;
+            return false; // couldn't find constant
         }
     }
 }
